200c941579e5359441ae57dde4bf1793ce161365
MiikaMatias <100348027+MiikaMatias@users.noreply.github.com>
2022-04-28 17:52:29 +0300

    General update
    Added powerup anims, animated small enemies, other stuff

---
+++ MinimalismProject/Assets/Audiomanager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Audiomanager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audio;
+    private AudioSource audioObject;
+
+    private float basevolume = 0;
+
+    void Awake()
+    {
+        audioObject = gameObject.GetComponent<AudioSource>();
+        if (gameObject.CompareTag("Melody"))
+        {
+            StartCoroutine(StartMelody());
+        }
+        if(gameObject.CompareTag("Spice"))
+        {
+            StartCoroutine(PlaySpice());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioObject.volume = basevolume * VolumeModifiers.masterSound * VolumeModifiers.music;
+    }
+
+    private IEnumerator StartMelody()
+    {
+        yield return new WaitForSeconds(28f);
+        StartCoroutine(FadeIn(0.1f));
+        audioObject.clip = audio[1];
+        audioObject.Play();
+    }
+
+    private IEnumerator PlaySpice()
+    {
+        yield return new WaitForSeconds(27.32f);
+        StartCoroutine(FadeIn(0.5f));
+        audioObject.clip = audio[2];
+        audioObject.Play();
+    }
+
+    private IEnumerator FadeIn(float i)
+    {
+        if(i >= 1) { basevolume = 1;  yield break; }
+        yield return new WaitForSeconds(0.3f);
+        i += 0.030f;
+        basevolume = i;
+        print(audioObject.volume);
+        StartCoroutine(FadeIn(i));
+    }
+}
--- MinimalismProject/Assets/RotationPowerup.cs
+++ MinimalismProject/Assets/RotationPowerup.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class RotationPowerup : MonoBehaviour
 {
+    private Vector3 offset = default;
+    private GameObject player;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         offset = player.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(
+                       Vector3.forward, // Keep z+ pointing straight into the screen.
+                       offset           // Point y+ toward the target.
+                     );
     }
+
+
+
+
 }
